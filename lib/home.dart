1dc828f0208fbe95362049664f6971fbf548ff37
Paulo Henrique Braga <paulo@someletras.com.br>
2020-09-09 17:40:42 -0300

    Create delete function and  New todo button and dialog

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/todoController.dart';
 import 'package:todo_list/todoModel.dart';
@@ -18,32 +19,95 @@ class _HomeState extends State<Home> {
   }
 
   _todoItem(TodoModel todoModel, TodoController todocontroller) {
-    return ListTile(
-      onLongPress: () {},
-      onTap: () {
-        print("clicou");
-        todocontroller.checkTodo(todoModel);
-      },
-      title: Text(todoModel.todo),
-      trailing: todoModel.isDone
-          ? Icon(
-              Icons.check_box,
-              color: Colors.green,
-            )
-          : Icon(
-              Icons.check_box_outline_blank,
-            ),
+    return Card(
+      elevation: 2,
+      child: ListTile(
+        onLongPress: () {
+          todocontroller.deleteTodo(todoModel);
+        },
+        onTap: () {
+          print("clicou");
+          todocontroller.checkTodo(todoModel);
+        },
+        title: Text(todoModel.todo),
+        trailing: todoModel.isDone
+            ? Icon(
+                Icons.check_box,
+                color: Colors.green,
+              )
+            : Icon(
+                Icons.check_box_outline_blank,
+              ),
+      ),
     );
+  }
+
+  void _showdialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancel", style: TextStyle(color: Colors.black)),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text(
+                  "Save",
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {},
+              )
+            ],
+            content: SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(1),
+                padding: EdgeInsets.only(bottom: 1.0),
+                height: 100,
+                width: 300,
+                child: TextField(
+                  //onChanged: (value) => ,
+                  maxLength: 50,
+                  textAlignVertical: TextAlignVertical.top,
+                  maxLines: null,
+                  minLines: null,
+                  expands: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            title: Text(
+              "New todo item",
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Todo List"),
-        ),
-        body: Consumer<TodoController>(builder: (context, todoController, widget) {
-          return _buildTodoList(todoController);
-        }));
+      backgroundColor: Colors.teal[50],
+      appBar: AppBar(
+        title: Text("To Do List"),
+      ),
+      body: Consumer<TodoController>(builder: (context, todoController, widget) {
+        return _buildTodoList(todoController);
+      }),
+      floatingActionButton: FloatingActionButton(
+        elevation: 20,
+        onPressed: () {
+          _showdialog();
+        },
+        // Add your ondiaPressed code here!
+
+        child: Icon(Icons.add),
+        backgroundColor: Colors.orange,
+      ),
+    );
   }
 }
--- lib/todoController.dart
+++ lib/todoController.dart
@@ -9,10 +9,27 @@ class TodoController extends ChangeNotifier {
     TodoModel(todo: "Test new program", isDone: false),
     TodoModel(todo: "Wash the car", isDone: false),
     TodoModel(todo: "Go to the gym", isDone: false),
+    TodoModel(todo: "Buy tomatoes", isDone: true),
+    TodoModel(todo: "Study Flutter", isDone: false),
+    TodoModel(todo: "Pay accounts", isDone: false),
+    TodoModel(todo: "Test new program", isDone: false),
+    TodoModel(todo: "Wash the car", isDone: false),
+    TodoModel(todo: "Go to the gym", isDone: false),
+    TodoModel(todo: "Buy tomatoes", isDone: true),
+    TodoModel(todo: "Study Flutter", isDone: false),
+    TodoModel(todo: "Pay accounts", isDone: false),
+    TodoModel(todo: "Test new program", isDone: false),
+    TodoModel(todo: "Wash the car", isDone: false),
+    TodoModel(todo: "Go to the gym", isDone: false),
   ];
 
   checkTodo(TodoModel todoModel) {
     todoModel.isDone = !todoModel.isDone;
+    notifyListeners();
+  }
+
+  deleteTodo(TodoModel todoModel) {
+    todos.remove(todoModel);
     notifyListeners();
   }
 }
